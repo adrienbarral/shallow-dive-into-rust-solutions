b3658995a9823f60d7b9d6e71c60b0b90d98cc39
Adrien <adrien.barra@exail.com>
2024-03-24 23:50:05 +0100

    Add ex8

--- ex3/src/main.rs
+++ ex3/src/main.rs
@@ -1,9 +1,12 @@
-fn say_hello(name: String) {
+fn say_hello(name: &String) {
     println!("Hello  {} ! ", name);
 }
 
 fn get_full_name(first_name: &String, given_name: &String) -> String {
-    // Look into the documentation of String, it may exists something to append a string to another.  
+    // Look into the documentation of String, it may exists something to append a string to another.
+
+    let full_name = [first_name.clone(), given_name.clone()].join(" ");
+    let full_name2 = [first_name, " ", given_name].concat();
     let mut res = first_name.clone();
     res.push_str(" ");
     res.push_str(given_name);
@@ -13,21 +16,18 @@ fn get_full_name(first_name: &String, given_name: &String) -> String {
 fn main() {
     println!("Enter your First Name : ");
     let first_name: String = text_io::read!("{}\n");
-    
-    say_hello(first_name);
 
-    /*
+    say_hello(&first_name);
+
     // Exercice : Uncomment this and make this program compile
-    
+
     println!("Enter your Given Name : ");
     let given_name: String = text_io::read!("{}\n");
 
     let full_name = get_full_name(&first_name, &given_name);
 
     println!("Nice to meet you {}", full_name);
-    */
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -35,7 +35,10 @@ mod tests {
 
     #[test]
     fn ex3_can_generate_full_name() {
-        // Fill this test... I want to check that given a first name : BARRAL, and a given name Adrien, the function return "BARRAL Adrien"        
-        assert_eq!(get_full_name(&String::from("BARRAL"), &String::from("Adrien")), "BARRAL Adrien");
+        // Fill this test... I want to check that given a first name : BARRAL, and a given name Adrien, the function return "BARRAL Adrien"
+        assert_eq!(
+            get_full_name(&String::from("BARRAL"), &String::from("Adrien")),
+            "BARRAL Adrien"
+        );
     }
 }
---
+++ ex8/src/main.rs
@@ -0,0 +1,68 @@
+struct Person {
+    name: String,
+    age: u8,
+}
+
+impl Ord for Person
+{
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.age.cmp(&other.age)
+
+    }
+}
+
+impl PartialEq for Person {
+    fn eq(&self, other: &Self) -> bool {
+        self.age == other.age
+    }
+}
+
+impl Eq for Person {}
+
+impl PartialOrd for Person {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn main() {
+    
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_we_can_sort_persons() {
+        let simpsons = vec![
+            Person {
+                name: "Bart".to_string(),
+                age: 10,
+            },
+            Person {
+                name: "Homer".to_string(),
+                age: 39,
+            },
+            Person {
+                name: "Marge".to_string(),
+                age: 38,
+            },
+            Person {
+                name: "Lisa".to_string(),
+                age: 8,
+            },
+            Person {
+                name: "Maggie".to_string(),
+                age: 1,
+            },
+        ];
+
+        let mut simpsons = simpsons;
+        simpsons.sort();
+        assert_eq!(simpsons[0].name, "Maggie");
+        assert_eq!(simpsons[1].name, "Lisa");
+        assert_eq!(simpsons[2].name, "Bart");
+        assert_eq!(simpsons[3].name, "Marge");
+        assert_eq!(simpsons[4].name, "Homer");
+    }
+}
